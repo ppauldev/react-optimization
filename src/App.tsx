e95966d6f99ecf574e58a19419ec85fa9fc7571a
Phillip Paul <ppaul_dev@t-online.de>
2023-04-11 23:06:08 +0200

    Add initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import { lorem } from './constants/text'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="flex flex-col">
+      {/* <!--Tabs navigation--> */}
+      <ul
+        className="mb-5 flex justify-center list-none flex-row flex-wrap border-b-0 pl-0"
+        role="tablist">
+        <li role="presentation">
+          <span
+            className={`my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 hover:text-violet-700 focus:isolate focus:border-transparent cursor-pointer ${mode === 0 ? 'bg-neutral-100' : ''}`}
+            role="tab"
+            onClick={() => setMode(0)}
+          >
+            Home
+          </span>
+        </li>
+        <li role="presentation">
+          <span
+            className={`my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 hover:text-violet-700 focus:isolate focus:border-transparent cursor-pointer ${mode === 1 ? 'bg-neutral-100' : ''}`}
+            role="tab"
+            onClick={() => setMode(1)}
+          >
+            Profile
+          </span>
+        </li>
+        <li role="presentation">
+          <span
+            className={`my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 hover:text-violet-700 focus:isolate focus:border-transparent cursor-pointer ${mode === 2 ? 'bg-neutral-100' : ''}`}
+            role="tab"
+            onClick={() => setMode(2)}
+          >
+            Messages
+          </span>
+        </li>
+      </ul>
+
+      {/* <!--Tabs content--> */}
+      {mode === 0 && (<Content0 />)}
+      {mode === 1 && (<Content1 />)}
+      {mode === 2 && (<Content2 />)}
+    </div>
+  )
+}
+
+const Content0 = () => {
+  return (
+    <div id="content-0" className="mt-6 mb-6">
+      {lorem.split("").map((char: string) => <span>{char}</span>)}
+    </div>
+  )
+}
+
+const Content1 = () => {
+  return (
+    <div id="content-1" className="mt-6 mb-6">
+      {lorem.split("").map((char: string) => <>{char}</>)}
+    </div>
+  )
+}
+
+const Content2 = () => {
+  return (
+    <div id="content-2" className="mt-6 mb-6">
+      {lorem}
     </div>
   )
 }
